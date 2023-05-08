15c3d468612140ac9d0aea9700bed7ab6b135a1c
DaveHdz <76536169+DaveHdz13@users.noreply.github.com>
2023-05-08 00:02:25 -0600

    Avatar size correction

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,6 +3,8 @@ import './home.css';
 import avatar02 from '../../assets/Avatar02.png';
 import TerminalApp from "../TerminalApp/TerminalApp";
 
+// The 'avatar' is going to be a 3D model that interacts when hover :D
+
 function Home() {
   return (
     <section className="home-section">
