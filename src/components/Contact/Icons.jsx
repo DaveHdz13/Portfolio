cb61e2e458c196aef9a79e5a180bc93916c15ce5
DaveHdz <76536169+DaveHdz13@users.noreply.github.com>
2023-06-17 14:38:22 -0600

    Icons animation and display text func

--- src/components/Contact/Icons.jsx
+++ src/components/Contact/Icons.jsx
@@ -1,22 +1,75 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { gsap } from "gsap";
-import './icons.css'
+import "./icons.css";
+import { Draggable } from "gsap/Draggable";
+
+gsap.registerPlugin(Draggable);
 
 function Icons() {
   const compRef = useRef(null);
 
   useEffect(() => {
-    const icons = compRef.current.querySelectorAll(".whats-icon, .linkedIn-icon, .gmail-icon");
+    const icons = compRef.current.querySelectorAll(
+      ".whats-icon, .linkedIn-icon, .gmail-icon"
+    );
+
     gsap.set(icons, { transformOrigin: "center center" });
-    gsap.from(icons, { rotation: 360, duration: 1, ease: "power3.out", stagger: 0.2 });
+
+    Draggable.create(icons, {
+      type: "x,y",
+      bounds: ".icons-section",
+      edgeResistance: 0.65,
+      onDrag: function () {
+        gsap.to(this.target, {
+          x: this.x,
+          y: this.y,
+          rotation: 0,
+          duration: 0.5,
+        });
+      },
+      onThrowUpdate: function () {
+        gsap.to(this.target, {
+          x: this.x,
+          y: this.y,
+          rotation: 0,
+          duration: 0.5,
+        });
+      },
+      onThrowComplete: function () {
+        gsap.to(this.target, {
+          x: this.x,
+          y: this.y,
+          rotation: Math.random() * 720 - 360,
+          ease: "back.out(1.7)",
+          duration: 1,
+        });
+      },
+    });
   }, []);
 
+  const [textDisplay, setTextDisplay] = useState('')
+  const data = ['55-2295-7804', 'DaveHdz', 'davehdz.111@gmail.com']
+
+  function changeDisplay(e) {
+    if (e.target.id === "whats") {
+      console.log(data[0]);
+      setTextDisplay(data[0])
+    } else if (e.target.id === "linked") {
+      console.log(data[1]);
+      setTextDisplay(data[1])
+    } else if (e.target.id === "gmail") {
+      console.log(data[2]);
+      setTextDisplay(data[2])
+    }
+  }
   return (
     <div className="icons-section" ref={compRef}>
-      <div className="whats-icon"></div>
-      <div className="display-contact">Texto</div>
-      <div className="linkedIn-icon"></div>
-      <div className="gmail-icon"></div>
+      <div id="whats" className="whats-icon" onClick={changeDisplay}></div>
+      <div className="display-contact">
+        {textDisplay}
+      </div>
+      <div id="linked" className="linkedIn-icon" onClick={changeDisplay}></div>
+      <div id="gmail" className="gmail-icon" onClick={changeDisplay}></div>
     </div>
   );
 }
